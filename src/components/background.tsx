2d132169441e4f5662c2d651cc9ab7d1bd80181f
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-17 21:56:13 +0900

    commit

---
+++ src/components/background.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Background() {
+  const [imagePath, setImagePath] = useState<string>("");
+
+  useEffect(() => {
+    const randomImageNumber = Math.floor(Math.random() * 24) + 1;
+    import(`../assets/images/${randomImageNumber}.jpg`)
+      .then((image) => setImagePath(image.default))
+      .catch((err) => console.error("Failed to load image", err));
+  }, []);
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${imagePath})`,
+        height: "100vh",
+        width: "100vw",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/* Your homepage content goes here */}
+    </div>
+  );
+}
+
+export default Background;
