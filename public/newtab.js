2f70db948cdb5a15a5620e2d7f62eb7a21bcbd5f
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-17 22:35:39 +0900

    commit

--- public/newtab.js
+++ public/newtab.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Background from "./components/background";
+import App from "./App";
 ReactDOM.render(React.createElement(React.StrictMode, null,
-    React.createElement(Background, null)), document.getElementById("root"));
+    React.createElement(App, null)), document.getElementById("root"));
--- src/newtab.tsx
+++ src/newtab.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Background from "./components/background";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Background />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
